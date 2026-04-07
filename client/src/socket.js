b17ffe6c53031631d98d025018b89a6f8d02d0a9
PrinceLegitTV <osagieirabor41@gmail.com>
2026-04-07 13:16:37 -0400

    Fix socket connection for mobile

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,11 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+const SOCKET_URL =
+  import.meta.env.VITE_SOCKET_SERVER_URL ??
+  "https://konnect4-backend.onrender.com";
 
 export const socket = io(SOCKET_URL, {
+  autoConnect: false,
   withCredentials: true,
-transports: ["websocket", "polling"],
+  transports: ["websocket", "polling"],
 });
