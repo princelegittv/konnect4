2e8c6b97f5c5e132c78ea33cb217ff14d6f37400
PrinceLegitTV <osagieirabor41@gmail.com>
2026-04-07 15:40:46 -0400

    Fix production socket connection

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,12 +1,12 @@
 import { io } from "socket.io-client";
 
-const SOCKET_URL =
-  import.meta.env.DEV
-    ? "http://localhost:3001"
-    : "";
+const SOCKET_URL = import.meta.env.DEV
+  ? "http://localhost:3001"
+  : window.location.origin;
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   withCredentials: true,
   transports: ["websocket", "polling"],
+  path: "/socket.io",
 });
