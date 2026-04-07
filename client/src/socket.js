e9a831ac09055653dd3d53968a4da7da72fcc916
PrinceLegitTV <osagieirabor41@gmail.com>
2026-04-07 16:21:12 -0400

    Revert socket config

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,11 +2,11 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.DEV
   ? "http://localhost:3001"
-  : "";
+  : "https://konnect4-backend.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   withCredentials: true,
-  transports: ["polling"],
+  transports: ["polling", "websocket"],
   path: "/socket.io",
 });
