416afe222cb42940ddbf9786633ea23782eb3562
PrinceLegitTV <osagieirabor41@gmail.com>
2026-04-07 16:09:22 -0400

    Use polling first for authenticated sockets

--- client/src/socket.js
+++ client/src/socket.js
@@ -7,6 +7,6 @@ const SOCKET_URL = import.meta.env.DEV
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
   withCredentials: true,
-  transports: ["websocket", "polling"],
+  transports: ["polling", "websocket"],
   path: "/socket.io",
 });
