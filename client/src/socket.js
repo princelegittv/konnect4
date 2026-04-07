6e3f9c4f1f467b548bf5c733354cd9cb42fc744e
PrinceLegitTV <osagieirabor41@gmail.com>
2026-04-07 15:46:05 -0400

    Connect sockets directly to backend

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const SOCKET_URL = import.meta.env.DEV
   ? "http://localhost:3001"
-  : window.location.origin;
+  : "https://konnect4-backend.onrender.com";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
