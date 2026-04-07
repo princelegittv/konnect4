b02bf5b4c57c14c1121e337295a3ddf1cdf581ba
PrinceLegitTV <osagieirabor41@gmail.com>
2026-04-07 03:33:37 -0400

    Fix Safari socket support

--- client/src/socket.js
+++ client/src/socket.js
@@ -4,4 +4,5 @@ const SOCKET_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
 
 export const socket = io(SOCKET_URL, {
   withCredentials: true,
+transports: ["websocket", "polling"],
 });
